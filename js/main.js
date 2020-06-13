15433d7c8dee5133f2d39cb26ebb2a85ac494256
SerhiiSerhiienko <sergusasss@gmail.com>
2020-06-13 21:25:53 +0300

    Final Project

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let canvas = document.getElementById("myCanvas");
+let ctx = canvas.getContext("2d");
+ctx.font = "25px Arial";
+ctx.fillText("Hello Word!", 75, 45);
+
+document.getElementById('demo').innerHTML = "Знання створюють наше майбутнє!";
+
